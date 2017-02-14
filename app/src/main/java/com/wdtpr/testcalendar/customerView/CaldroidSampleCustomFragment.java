f6acd822766012f1a272183d471c9f981836b1a9
augusWuDotan <popo124117@gmail.com>
2017-02-14 22:23:46 +0800

    test1
    123456

--- app/src/main/java/com/wdtpr/testcalendar/customerView/CaldroidSampleCustomFragment.java
+++ app/src/main/java/com/wdtpr/testcalendar/customerView/CaldroidSampleCustomFragment.java
@@ -13,6 +13,8 @@ public class CaldroidSampleCustomFragment  extends CaldroidFragment {
     @Override
     public CaldroidGridAdapter getNewDatesGridAdapter(int month, int year) {
         return new CaldroidSampleCustomAdapter(getActivity(),month,year,getCaldroidData(),extraData);
+        //
+        //
     }
 
 
